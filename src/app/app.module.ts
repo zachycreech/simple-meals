ae44717040716680b139ca757d8ea9844a97ff82
Wilder <SaveTQ@gmail.com>
2021-07-30 23:51:01 -0400

    New: Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './components/button/button.component';
 import { RandomMealComponent } from './components/random-meal/random-meal.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     ButtonComponent,
     RandomMealComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
