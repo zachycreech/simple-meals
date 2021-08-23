c473fae1f48a2894285c47770d4b4ba0d51ad14a
Zach <zwheele1@gmail.com>
2021-08-23 16:54:05 -0400

    New: Button Logic
    Adds button logic for onClick events

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+// example
+//  ----html----
+//  <app-button text="Refresh" color="black" (btnClick)="onRefresh()"></app-button>
+//   ----ts----
+//   onRefresh() {
+//     this.ngOnInit()
+//   }
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  styleUrls: ['./button.component.css'],
 })
-export class ButtonComponent implements OnInit {
+export class ButtonComponent {
+  @Input() text?: string;
+  @Input() color: string = "rgb(55, 0, 158)";
+  @Output() btnClick = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  onClick(): void {
+    this.btnClick.emit();
   }
-
 }
